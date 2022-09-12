24ac9a539f8a6b5a1e47f612a4c8fedf5f275d1f
Aline Tavares <aline@crowdbotics.com>
2022-09-12 10:56:27 -0400

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,42 +1,50 @@
+import { StyleSheet } from "react-native";
 import { Text } from "react-native";
 import React from "react";
 import { View } from "react-native";
 
 const Untitled1 = () => {
-  return <View style={{
-    backgroundColor: '#f0f0f1',
-    padding: 10,
-    position: 'relative',
-    height: '100%'
-  }}>
-      <Text style={{
-      left: 51,
-      top: 54,
-      position: "absolute",
-      width: 100,
-      height: 50,
-      lineHeight: 14,
-      fontSize: 14,
-      borderRadius: 0
-    }}>Lorem ipsum… </Text><View style={{
-      left: 18,
-      top: 76,
-      position: "absolute",
-      height: 60,
-      width: 140,
-      backgroundColor: "#E4E4E4",
-      borderRadius: 0,
-      color: "#777777"
-    }}></View><View style={{
-      left: 217,
-      top: 101,
-      position: "absolute",
-      height: 60,
-      width: 140,
-      backgroundColor: "#E4E4E4",
-      borderRadius: 0,
-      color: "#777777"
-    }}></View></View>;
+  return <View style={_styles.nHqPYbte}>
+      <Text style={_styles.YdhFHJdy}>Lorem ipsum… </Text><View style={_styles.HKJqvdrI}></View><View style={_styles.yjrWUsww}></View></View>;
 };
 
 export default Untitled1;
+
+const _styles = StyleSheet.create({
+  nHqPYbte: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  },
+  YdhFHJdy: {
+    left: 51,
+    top: 54,
+    position: "absolute",
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  },
+  HKJqvdrI: {
+    left: 18,
+    top: 76,
+    position: "absolute",
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  yjrWUsww: {
+    left: 217,
+    top: 101,
+    position: "absolute",
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  }
+});
