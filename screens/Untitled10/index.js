5241935026876f4da14a63c93d9103712271c0e7
Aline Tavares <aline@crowdbotics.com>
2022-09-21 10:14:08 -0400

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled10/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled10 = () => {
+  return <View style={_styles.hRVPOVwz}>
+      </View>;
+};
+
+export default Untitled10;
+
+const _styles = StyleSheet.create({
+  hRVPOVwz: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
