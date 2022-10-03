6c3ab3fa8f97de43502ac51dd7bcb22134708685
Aline Tavares <aline@crowdbotics.com>
2022-10-03 15:18:15 -0400

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled15/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled15 = () => {
+  return <View style={_styles.TjtDoFab}>
+      <View style={_styles.XOuANVop}></View></View>;
+};
+
+export default Untitled15;
+
+const _styles = StyleSheet.create({
+  TjtDoFab: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  },
+  XOuANVop: {
+    left: 47,
+    top: 184,
+    position: "absolute",
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  }
+});
