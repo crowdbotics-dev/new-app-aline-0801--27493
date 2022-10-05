ef62a2ba00158587ab091ca3e8dcfdcb1a3a36a3
Aline Tavares <aline@crowdbotics.com>
2022-10-05 10:09:30 -0400

    Crowdbotics Studio: Update app screens

--- screens/Untitled14/index.js
+++ screens/Untitled14/index.js
@@ -1,9 +1,10 @@
+import { Pressable } from "react-native";
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
 const Untitled14 = () => {
   return <View style={_styles.FPKXTUmD}>
-      </View>;
+      <Pressable onPress={() => navigation.navigate("Untitled12Copy")} style={_styles.RUyPsKyy}><View style={_styles.EevBmvdH}></View></Pressable></View>;
 };
 
 export default Untitled14;
@@ -14,5 +15,18 @@ const _styles = StyleSheet.create({
     padding: 10,
     position: "relative",
     height: "100%"
+  },
+  EevBmvdH: {
+    left: 60,
+    top: 60,
+    position: "absolute",
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  RUyPsKyy: {
+    position: "unset"
   }
 });
---
+++ screens/Untitled6/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled6 = () => {
+  return <View style={_styles.jYyXxauZ}>
+      </View>;
+};
+
+export default Untitled6;
+
+const _styles = StyleSheet.create({
+  jYyXxauZ: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
