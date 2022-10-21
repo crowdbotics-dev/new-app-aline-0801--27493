b1fa86a5eb2d89066e4405da2e4774f9a1088b65
Aline Tavares <aline@crowdbotics.com>
2022-10-21 14:13:53 -0400

    Crowdbotics Studio: Update app screens

--- screens/Untitled7/index.js
+++ screens/Untitled7/index.js
@@ -1,18 +1,45 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
-const Untitled7 = () => {
-  return <View style={_styles.fTLzevQo}>
+const Untitled11 = () => {
+  return <View style={_styles.XBAJQHvv}>
+    <View style={_styles.wajpyxhG}>
+      <View style={_styles.zcRjNsoC}></View>
+      <View style={_styles.SzYJtDzv}></View>
+      <View style={_styles.MQTKlBWm}></View>
+    </View>
       </View>;
 };
 
-export default Untitled7;
+export default Untitled11;
 
 const _styles = StyleSheet.create({
-  fTLzevQo: {
+  XBAJQHvv: {
     backgroundColor: "#f0f0f1",
     padding: 10,
     position: "relative",
     height: "100%"
+  },
+  wajpyxhG: {
+    borderWidth: 2,
+    display: "block",
+    flexDirection: "row",
+    alignItems: "end",
+    justifyContent: "space-evenly"
+  },
+  zcRjNsoC: {
+    width: 50,
+    height: 50,
+    backgroundColor: "lightpink"
+  },
+  SzYJtDzv: {
+    width: 50,
+    height: 50,
+    backgroundColor: "lightblue"
+  },
+  MQTKlBWm: {
+    width: 50,
+    height: 50,
+    backgroundColor: "lightgreen"
   }
 });
