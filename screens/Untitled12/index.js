00c9cd6fc85a3cf731a4d1ae48293d4db4ceff49
Aline Tavares <aline@crowdbotics.com>
2022-11-15 15:16:45 -0500

    Crowdbotics Studio: Update app screens

--- screens/Untitled12/index.js
+++ screens/Untitled12/index.js
@@ -1,9 +1,10 @@
+import { Text } from "react-native";
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
 const Untitled12 = () => {
   return <View style={_styles.JJelZtJA}>
-      </View>;
+      <View style={_styles.XOMkMtqz}></View><Text style={_styles.EMVRkJCv}>Lorem ipsum…</Text></View>;
 };
 
 export default Untitled12;
@@ -14,5 +15,27 @@ const _styles = StyleSheet.create({
     padding: 10,
     position: "relative",
     height: "100%"
+  },
+  XOMkMtqz: {
+    left: 109,
+    top: 36,
+    position: "absolute",
+    height: 134,
+    width: 140,
+    backgroundColor: "#53b8f1",
+    borderRadius: "50%",
+    color: "#777777",
+    borderWidth: 2,
+    opacity: 0.82
+  },
+  EMVRkJCv: {
+    left: 135,
+    top: 94,
+    position: "absolute",
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 50
   }
 });
